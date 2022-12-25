561f5f3969e3be836dea619842e87a7367dcc4fe
pkr187 <pankajroman187@gmail.com>
2022-12-25 23:12:38 +0530

    [pankaj] Add:FibonacciSeries

---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to logical programming");
+            while (true)
+            {
+                Console.WriteLine("Select program\n 1. fibonacci series");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                        fibonacciSeries.Series();
+                        break;
+                    default:
+                        Console.WriteLine("Select correct option");
+                        break;
+                }
+            }
+        }
+    }
+}
