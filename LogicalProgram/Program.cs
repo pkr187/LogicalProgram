4754bf96b7030df4b0d30b72053fdee95f2746bd
pkr187 <pankajroman187@gmail.com>
2022-12-26 11:44:15 +0530

    [pankaj] Add:CouponNumbers

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -6,13 +6,13 @@
         {
             Console.WriteLine("Welcome to logical programming");
            
-                Console.WriteLine("Select program\n 1.TemperatureConversion");
+                Console.WriteLine("Select program\n 1.CouponNumbers");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        TemperatureConversion temperatureConversion = new TemperatureConversion();
-                        temperatureConversion.Conversion();
+                    CouponNumbers couponNumbers = new CouponNumbers();
+                    couponNumbers.DistinctCouponNumber();
                         break;
                     default:
                         Console.WriteLine("Select correct option");
