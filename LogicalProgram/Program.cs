f03b68d5ff44cd0d0a8821fee0585c541a0e4635
pkr187 <pankajroman187@gmail.com>
2022-12-27 00:44:36 +0530

    [pankaj] Add:MonthlyPayment

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -5,13 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to logical programming");
-                Console.WriteLine("Select program\n 1.Dayoftheweek");
+                Console.WriteLine("Select program\n 1.MonthlyPayment");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                    Dayoftheweek dayoftheweek = new Dayoftheweek();
-                    dayoftheweek.Days();
+                    MonthlyPayment monthlyPayment = new MonthlyPayment();
+                    monthlyPayment.Payment();
                         break;
                     default:
                         Console.WriteLine("Select correct option");
